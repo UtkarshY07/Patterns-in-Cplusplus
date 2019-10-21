634e51aeb9135dc8988ae3f90db3c20a0035649c
Utkarsh Yadav <utkaiyadav@gmail.com>
2019-10-21 11:29:40 +0530

    Pattern 16 added

---
+++ pattern16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the no of rows: ";
+	cin>>n;
+	for ( int i =1 ; i<=n ; i++)
+	{
+		int k=0;
+		while( k != n-i)
+		{
+			cout<<" ";
+			k++;
+		}
+		for ( int j=1; j<= 2*i-1;j++ )
+		{
+			if ( (j==1) || (j==2*i-1) )
+				cout<<i;
+			else
+				cout<<"0";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
